5fe3617b5acc715b028128e76aa31dd12bc98709
Akanksha Malhotra <akankshamalhotra24@gmail.com>
2018-09-06 22:43:01 +0530

    Updated package details

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     name='pypexels',
     version='1.0.0b1',
     packages=['pypexels', 'pypexels.src'],
-    url='https://github.com/salvoventura/pypexels',
+    url='https://github.com/AkankshaMalhotra/pypexels/',
     license='MIT',
-    author='salvatore ventura',
-    author_email='salvoventura@gmail.com',
-    description='A Python wrapper for the Pexels.com REST API',
+    author='akanksha malhotra',
+    author_email='akankshamalhotra24@gmail.com',
+    description='A 3.5 Python wrapper for the Pexels.com REST API',
     classifiers=[
                       # How mature is this project? Common values are
                       #   3 - Alpha
@@ -25,10 +25,10 @@ setup(
 
                       # Specify the Python versions you support here. In particular, ensure
                       # that you indicate whether you support Python 2, Python 3 or both.
-                      'Programming Language :: Python :: 2',
-                      'Programming Language :: Python :: 2.7',
+                      'Programming Language :: Python :: 3',
+                      'Programming Language :: Python :: 3.5',
                   ],
     keywords=['pexels', 'rest', 'api', 'python', 'wrapper', 'development', 'pexels.com', 'photography'],
     install_requires=['requests'],
-    python_requires='>=2.7, <3'
+    python_requires='>=3'
 )
